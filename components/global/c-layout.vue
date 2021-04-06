3fa72fa51c9247f585eea13ccf0f9d1f19d399bf
Nho-Ho <nho.ho@rikai.technology>
2021-04-06 07:34:13 +0700

    implement vee-validate, vuex-presistedstate, login screen

---
+++ components/global/c-layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-container class="c-layout" :style="{'height': centerFillHeight? '90vh': undefined}">
+      <v-row v-if="center" align="center" justify="center" :class="{'fill-height': centerFillHeight}">
+        <v-col>
+          <v-card width="720" class="mx-auto mt-3" flat>
+            <slot />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    center: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    centerFillHeight: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,10 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/persistedstate',
+    '~/plugins/vee-validate',
   ],
+
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
@@ -108,6 +111,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ["vee-validate/dist/rules"],
   },
   server: {
     port: 8080, // default: 3000
