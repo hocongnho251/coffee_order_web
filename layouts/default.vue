41ec0b2ff33856b44577b3abe100e874dda44b71
Nho-Ho <nho.ho@rikai.technology>
2021-05-08 20:13:58 +0700

    disable hoem page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-app
-    app
-  >
+  <v-app app>
     <v-app-bar v-if="isLogged" app color="#F0623D">
       <v-container>
         <div class="text-right white--text font-weight-bold">
@@ -26,16 +24,14 @@
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title class="#303030--text font-weight-bold">{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="#303030--text font-weight-bold">{{
+              item.title
+            }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-divider color="#303030"></v-divider>
-        <v-btn
-          block
-          @click.stop="logout"
-          color="#F8EFE5"
-        >
-          {{$t("admin.logout")}}
+        <v-btn block @click.stop="logout" color="#F8EFE5">
+          {{ $t("admin.logout") }}
         </v-btn>
         <v-divider color="#303030"></v-divider>
       </v-list>
@@ -45,40 +41,60 @@
         <nuxt />
       </c-main-scroll>
     </v-main>
-    <v-footer
-      absolute
-      app
-    >
-    </v-footer>
+    <v-footer absolute app> </v-footer>
   </v-app>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(['isLogged'])
+    ...mapGetters(["isLogged"])
   },
-  data () {
+  data() {
     return {
       items: [
-          { title: this.$i18n.t("admin.home"), icon: 'mdi-home-city', to: '/' },
-          { title: this.$i18n.t("admin.staff"), icon: 'mdi-account', to: '/staff' },
-          { title: this.$i18n.t("admin.customer"), icon: 'mdi-account-group-outline', to: '/customer' },
-          { title: this.$i18n.t("admin.table"), icon: 'mdi-table-large', to: '/table' },
-          { title: this.$i18n.t("admin.menu"), icon: 'mdi-account-group-outline', to: '/menu' },
-          { title: this.$i18n.t("admin.table_manager"), icon: 'mdi-account-group-outline', to: '/tables' },
-          { title: this.$i18n.t("admin.coupon"), icon: 'mdi-account-group-outline', to: '/coupon' },
-          { title: this.$i18n.t("admin.statistics"), icon: 'mdi-account-group-outline', to: '/order' },
-        ],
-    }
+        // { title: this.$i18n.t("admin.home"), icon: 'mdi-home-city', to: '/' },
+        {
+          title: this.$i18n.t("admin.staff"),
+          icon: "mdi-account",
+          to: "/staff"
+        },
+        {
+          title: this.$i18n.t("admin.customer"),
+          icon: "mdi-account-group-outline",
+          to: "/customer"
+        },
+        {
+          title: this.$i18n.t("admin.table"),
+          icon: "mdi-table-large",
+          to: "/table"
+        },
+        {
+          title: this.$i18n.t("admin.menu"),
+          icon: "mdi-account-group-outline",
+          to: "/menu"
+        },
+        {
+          title: this.$i18n.t("admin.table_manager"),
+          icon: "mdi-account-group-outline",
+          to: "/tables"
+        },
+        {
+          title: this.$i18n.t("admin.coupon"),
+          icon: "mdi-account-group-outline",
+          to: "/coupon"
+        }
+        // { title: this.$i18n.t("admin.statistics"), icon: 'mdi-account-group-outline', to: '/order' },
+      ]
+    };
   },
   methods: {
-    logout(){
-      this.$router.push({name: 'login'})
-      this.$store.commit("SET_LOGGED", false)
+    logout() {
+      this.$router.push({ name: "login" });
+      this.$store.commit("SET_LOGGED", false);
     }
   }
-}
+};
 </script>
